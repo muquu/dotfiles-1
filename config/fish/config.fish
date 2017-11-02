02cb39e1fd0637cbfd64b46a0d5f20f05694deee
muquu <muquu@skytrum.com>
2017-11-03 02:28:20 +0900

    ローカル設定を読み込むようにした

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,10 +1,13 @@
+# fish_greeting はしない
 function fish_greeting
 end
 
+# alias nvim
 function vi
   nvim $argv
 end
 
+# alias keychain
 function kc
   if status --is-interactive
     keychain --eval --quiet -Q $argv;
@@ -12,10 +15,15 @@ function kc
   end
 end
 
+# alias npm list global packages
 function ngl
   npm list -g --depth=0 2>/dev/null
 end
 
+# alias npm list local packages
 function nll
   npm list --depth=0 2>/dev/null
 end
+
+# local setting
+source $HOME/.local.fish
